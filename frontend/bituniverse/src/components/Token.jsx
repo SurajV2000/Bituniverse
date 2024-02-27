f7d81e0dfe1848e59c6660cee390d30904bab07d
SurajV2000 <sv107719@gmail.com>
2024-02-27 23:52:58 +0530

    done

--- frontend/bituniverse/src/components/Token.jsx
+++ frontend/bituniverse/src/components/Token.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import bit from "../assests/bit.png"
-import "../css/token.css"
+import React, { useEffect, useState } from "react";
+import bit from "../assests/bit.png";
+import "../css/token.css";
 
 const Token = () => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      let tok = await fetch("https://backend-bituniverse.onrender.com/token");
+      let token = await tok.json();
+      setData(token);
+      // console.log(token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <p style={{ color: "white", fontSize: "56px", fontWeight: "600" }}>
+      <p id="head" style={{ color: "white", fontSize: "56px", fontWeight: "600" }}>
         Our <span style={{ color: "#FECC00" }}>Tokens</span>
       </p>
       <p
@@ -22,84 +39,40 @@ const Token = () => {
         habitant morbi.
       </p>
       <div id="container">
-        <div className="token">
-          <img src={bit} alt="bit" />
-          <div>
-            <div>
-              <p>Bitcoin</p>
+        {data?.map((el,id) => {
+          return (
+            <div className="token">
+              <img src={el.img} alt="bit" />
+              <div>
+                <div>
+                  <p>{el.tokenName}</p>
+                </div>
+                <div className="data">
+                  <p>Symbol</p>
+                  <p>{el.symbol}</p>
+                </div>
+                <div className="data">
+                  <p>Decimal</p>
+                  <p>{el.decimals}</p>
+                </div>
+                <div className="data">
+                  <p>MarketCap</p>
+                  <p>{el.marketcap}</p>
+                </div>
+                <div className="data">
+                  <p>Chain</p>
+                  <p>{el.chain}</p>
+                </div>
+              </div>
+              <button>Details</button>
             </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-          </div>
-          <button>Details</button>
-        </div>
-        <div className="token">
-          <img src={bit} alt="bit" />
-          <div>
-            <div>
-              <p>Bitcoin</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-          </div>
-          <button>Details</button>
-        </div>
-        <div className="token">
-          <img src={bit} alt="bit" />
-          <div>
-            <div>
-              <p>Bitcoin</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-            <div className="data">
-              <p>Symbol</p>
-              <p>BTC</p>
-            </div>
-          </div>
-          <button>Details</button>
-        </div>
+          );
+        })}
+        
+
       </div>
     </div>
   );
-}
+};
 
-export default Token
+export default Token;
